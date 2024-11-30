87af45e5ed17b466850ef315ab3d7a28e3838207
Alejandro <agarcia@sensia-solutions.com>
2024-11-30 17:19:05 +0100

    <Fran> Add boolean to fade-in

--- Jam/Assets/Scripts/ScriptIntro.cs
+++ Jam/Assets/Scripts/ScriptIntro.cs
@@ -8,25 +8,35 @@ using UnityEngine.UI;
 
 public class ScriptIntro : MonoBehaviour
 {
- public TextMeshProUGUI textComponent;
- public float           fadeTime;
- private float           alphaValue;
- private float          fadeAwayPerSecond;
+    public TextMeshProUGUI textComponent;
+    public float           fadeTime;
+    private float           alphaValue;
+    private float          fadeAwayPerSecond;
+    private bool fadingOut = false;
 
-void Start()
-{
-    fadeAwayPerSecond = 1 / fadeTime;
-    alphaValue = textComponent.color.a;
-}
- void Update()
- {
-    if (alphaValue < 0)
-        SceneManager.LoadScene(1);
-    if (fadeTime > 0)
+    void Start()
     {
-        alphaValue -= fadeAwayPerSecond * Time.deltaTime;
-        textComponent.color = new UnityEngine.Color(textComponent.color.r, textComponent.color.g, textComponent.color.b, alphaValue);
-        fadeTime -= Time.deltaTime;
+        fadeAwayPerSecond = 1 / fadeTime;
+        alphaValue = textComponent.color.a;
     }
- }
+    void Update()
+    {
+        if(fadingOut)
+        {
+            if (alphaValue < 0)
+                SceneManager.LoadScene(1);
+                
+            if (fadeTime > 0)
+            {
+                alphaValue -= fadeAwayPerSecond * Time.deltaTime;
+                textComponent.color = new UnityEngine.Color(textComponent.color.r, textComponent.color.g, textComponent.color.b, alphaValue);
+                fadeTime -= Time.deltaTime;
+            }
+        }
+    }
+
+    public void setFadingOut(bool what)
+    {
+        fadingOut = what;
+    }
 }
