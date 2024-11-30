bc8eea8e034325db8f7fb9f0a9face31ca612a76
Fran Morán <franmoranmato@gmail.com>
2024-11-30 13:30:25 +0100

    Different scenes

--- Jam/Assets/ClickableObject.cs
+++ Jam/Assets/ClickableObject.cs
@@ -6,25 +6,37 @@ using UnityEngine.UI;
 
 public class ClickableObject : MonoBehaviour
 {
+	public SpriteRenderer Sprite;
 	public GameObject text;
 	public GameObject YesButton;
 	public GameObject NoButton;
 	bool	isDestroyed;
+	bool	isMenu;
+
+	bool	color;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+		Debug.Log(GlobalVariables.price);
 		DisableText();
 		isDestroyed = false;
+		color = false;
+		isMenu = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-		if (Input.GetMouseButtonDown(0))
+		if (color == false)
+			Sprite.color = Color.red;
+		else
+			Sprite.color = Color.white;
+	    Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+		CheckColor(hit);
+		if (Input.GetMouseButtonDown(0) && hit.collider)
 		{
-	        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        	RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
-
+			isMenu = true;
 			if (hit.collider && isDestroyed == false)
 				EnableText();
 		}
@@ -35,6 +47,7 @@ public class ClickableObject : MonoBehaviour
 		text.SetActive(false);
 		YesButton.SetActive(false);
 		NoButton.SetActive(false);
+		isMenu = false;
 	}
 	public void YesClick()
 	{
@@ -42,6 +55,9 @@ public class ClickableObject : MonoBehaviour
 		Destroy(YesButton);
 		Destroy(NoButton);
 		isDestroyed = true;
+		isMenu = false;
+		GlobalVariables.price -= 400;
+		Debug.Log(GlobalVariables.price);
 	}
 
 	void EnableText()
@@ -49,5 +65,21 @@ public class ClickableObject : MonoBehaviour
 		text.SetActive(true);
 		YesButton.SetActive(true);
 		NoButton.SetActive(true);
+	}
+
+	void CheckColor(RaycastHit2D hit)
+	{
+		if (!isDestroyed)
+		{
+			if (hit.collider && !isMenu)
+				color = true;
+			else if (isMenu)
+				color = true;
+			else
+				color = false;
+		}
+		else
+			color = false;
+
 	}
 }
