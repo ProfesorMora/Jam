98a38820b9ddca426cc288eee69cd623353cdf4e
Alejandro <agarcia@sensia-solutions.com>
2024-12-01 01:31:18 +0100

    <Alex> Form functionality and assets

--- Jam/Assets/formScript.cs
+++ Jam/Assets/formScript.cs
@@ -1,26 +1,102 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class formScript : MonoBehaviour
 {
     public GameObject formPanel;
     public GameObject submitButton;
-    public List<GameObject> textBoxesList;
-    public List<GameObject> buttonsList;
     public DialogManager dialogManager;
+    public List<TMP_InputField> listOfInputs;
+    public ToggleGroup toggleGroupPet; // Esto no lo pilla el formScript
+    public ToggleGroup toggleGroupWork; //
+
+    List<bool> listOfToggleBools;
+    List<int> listOfToggleValues;
+    int petSelection;
+    int bloodSelection;
+    int workSelection;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         formPanel.SetActive(false);
         submitButton.SetActive(false);
+        listOfToggleBools = new List<bool>{false, false, false};
+        listOfToggleValues = new List<int>{0,0,0};
     }
 
     // Update is called once per frame
     void Update()
     {
         if(dialogManager.deactivated) formPanel.SetActive(true);
+        if(isFormFilled()) submitButton.SetActive(true);
     }
 
-    
+    public bool isFormFilled()
+    {
+        foreach(var b in listOfToggleBools)
+        {
+            if(!b) return false;
+        }
+
+        return true;
+    }
+
+    public void assign(string where, int what)
+    {
+        if(where == "pet") petSelection = what;
+        if(where == "blood") bloodSelection = what;
+        if(where == "work") workSelection = what;
+    }
+
+    public void petSelected()
+    {
+        listOfToggleBools[0] = true;
+    }
+
+    public void bloodSelected()
+    {
+        listOfToggleBools[1] = true;
+    }
+
+    public void workSelected()
+    {
+        listOfToggleBools[2] = true;
+    }
+
+    public void submitForm()
+    {
+        Debug.Log("Will submit form");
+        calculatePrice();
+        changeScene();
+    }
+
+    void calculatePrice()
+    {
+        int currentPrice = 0;
+        foreach( Toggle toggle in toggleGroupPet.ActiveToggles() )
+        {
+            if(toggle.name == "togGatos") currentPrice -= 300;
+            if(toggle.name == "togPerros") currentPrice += 300;
+        }
+
+        foreach( Toggle toggle in toggleGroupWork.ActiveToggles() )
+        {
+            if(toggle.name == "togIndefinido") currentPrice -= 300;
+            if(toggle.name == "togTemporal") currentPrice += 300;
+        }
+
+        currentPrice -= 150;
+
+        Debug.Log("Current Price: " + currentPrice);
+
+    }
+
+    void changeScene()
+    {
+        Debug.Log("Will change scene");
+        //SceneManager.LoadScene(2);
+    }
 }
